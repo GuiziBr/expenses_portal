d9e4ee9bb134bfed04c7733188c8befbb8dc0cd5
Guizi <ricardo.guizi@ewally.com.br>
2020-12-28 15:56:27 -0300

    added apiClient and hook for authentication

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { FiMail, FiLock } from 'react-icons/fi'
 import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
+import { useAuth } from '../../hooks/AuthContext'
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg'
@@ -12,9 +13,15 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
-  const handleSubmit = useCallback(async (data: object) => {
+  const { signIn } = useAuth()
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({})
       const schema = Yup.object().shape({
@@ -22,11 +29,12 @@ const SignIn: React.FC = () => {
         password: Yup.string().required('Senha é obrigatória'),
       })
       await schema.validate(data, { abortEarly: false })
+      signIn({ email: data.email, password: data.password })
     } catch (err) {
       const errors = getValidationErrors(err)
       formRef.current?.setErrors(errors)
     }
-  }, [])
+  }, [signIn])
 
   return (
     <Container>
