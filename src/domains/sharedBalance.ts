be3d659c29c88ecd668022bfe490626ded9537d9
Ricardo Guizi <guizi189@gmail.com>
2022-08-17 19:54:59 -0700

    Add categories filter into shared balance dashboard

--- src/domains/sharedBalance.ts
+++ src/domains/sharedBalance.ts
@@ -4,10 +4,16 @@ interface IBank {
   total: number
 }
 
-interface IPayment {
+export interface IPayment {
   id: string
   description: string
   banks: Array<IBank>
+  total: number
+}
+
+export interface ICategory {
+  id: string
+  description: string
   total: number
 }
 
@@ -15,10 +21,11 @@ interface IReport {
   id : string
   name?: string
   payments?: Array<IPayment>
+  categories?: Array<ICategory>
   total: number
 }
 
-export interface IConsolidatedReport {
+export interface ISharedReport {
   requester: IReport,
   partner?: IReport,
   balance: number
@@ -26,4 +33,5 @@ export interface IConsolidatedReport {
 
 export interface IFormData {
   month: string
+  balanceType: string
 }
--- src/pages/ConsolidatedBalance/index.tsx
+++ src/pages/ConsolidatedBalance/index.tsx
@@ -2,6 +2,7 @@ import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import { AxiosRequestConfig } from 'axios'
 import React, { Fragment, useRef, useState } from 'react'
+import { HiOutlineSelector } from 'react-icons/hi'
 import { MdDateRange } from 'react-icons/md'
 import income from '../../assets/income.svg'
 import outcome from '../../assets/outcome.svg'
@@ -9,50 +10,93 @@ import total from '../../assets/total.svg'
 import Button from '../../components/Button'
 import Header from '../../components/Header'
 import Input from '../../components/Input'
+import Select from '../../components/Select'
 import constants from '../../constants/constants'
-import { IConsolidatedReport, IFormData } from '../../domains/consolidatedBalance'
+import { ISharedReport, IFormData, IPayment, ICategory } from '../../domains/sharedBalance'
 import api from '../../services/apiClient'
 import { formatAmount } from '../../utils/formatAmount'
 import { Card, CardContainer, Container, FormContainer, Table, TableContainer } from './styles'
 
-const ConsolidatedBalance: React.FC = () => {
+const SharedBalance: React.FC = () => {
   const [isDeskTopScreen] = useState<boolean>(window.innerWidth > 720)
-  const [consolidated, setConsolidated] = useState<IConsolidatedReport>()
+  const [sharedReport, setSharedReport] = useState<ISharedReport>()
   const formRef = useRef<FormHandles>(null)
+  const [shareType, setShareType] = useState<string>('')
 
-  const loadConsolidatedBalance = async (month: number): Promise<void> => {
+  const loadSharedBalance = async (month: number, balanceType: string): Promise<void> => {
     const token = sessionStorage.getItem(constants.sessionStorage.token)
     const config: AxiosRequestConfig = { headers: { Authorization: `Bearer ${token}` }}
     const { data } = await api.get(`/balance/consolidated/${month}`, config)
-    setConsolidated(data)
+    setShareType(balanceType)
+    setSharedReport(data)
   }
 
-  const handleSubmit = async ({ month: date }: IFormData) => {
-    if (!date) return
+  const handleSubmit = async ({ month: date, balanceType }: IFormData) => {
+    if (!date || !balanceType) return
     const month = Number(date.split('-')[1])
-    await loadConsolidatedBalance(month)
+    await loadSharedBalance(month, balanceType)
   }
+
+  const Payments = ({ payments, className }) => (
+    payments.map((payment: IPayment) => (
+      <Fragment key={payment.id}>
+        <tr>
+          <td className="payment-type" colSpan={2}>{payment.description}</td>
+        </tr>
+        {shareType === 'payments' && payment.banks.map((bank) => (
+          <tr key={bank.id}>
+            <td className="bank-name">{bank.name}</td>
+            <td className="bank-total">{formatAmount(bank.total)}</td>
+          </tr>
+        ))}
+        <tr>
+          <td className={className} colSpan={2}>
+            Total
+            {' - '}
+            {formatAmount(payment.total)}
+          </td>
+        </tr>
+      </Fragment>
+    ))
+  )
+
+  const Categories = ({ categories, className }) => (
+    categories.map((category: ICategory) => (
+      <Fragment key={category.id}>
+        <tr>
+          <td className="category-name">
+            {category.description}
+          </td>
+          <td className={`category-total ${className}`}>
+            Total
+            {' - '}
+            {formatAmount(category.total)}
+          </td>
+        </tr>
+      </Fragment>
+    ))
+  )
 
   return (
     <>
-      <Header current="ConsolidatedBalance" />
+      <Header current="SharedBalance" />
       <Container>
         <CardContainer>
           {isDeskTopScreen && (
             <>
               <Card>
                 <header>
-                  <p>{consolidated?.requester?.name || 'Requester'}</p>
+                  <p>{sharedReport?.requester?.name || 'Requester'}</p>
                   <img src={income} alt="Income" />
                 </header>
-                <h1>{formatAmount(consolidated?.requester.total || 0)}</h1>
+                <h1>{formatAmount(sharedReport?.requester.total || 0)}</h1>
               </Card>
               <Card>
                 <header>
-                  <p>{consolidated?.partner?.name || 'Partner'}</p>
+                  <p>{sharedReport?.partner?.name || 'Partner'}</p>
                   <img src={outcome} alt="Outcome" />
                 </header>
-                <h1>{formatAmount(consolidated?.partner?.total || 0)}</h1>
+                <h1>{formatAmount(sharedReport?.partner?.total || 0)}</h1>
               </Card>
             </>
           )}
@@ -61,79 +105,55 @@ const ConsolidatedBalance: React.FC = () => {
               <p>Balance</p>
               <img src={total} alt="Balance" />
             </header>
-            <h1>{formatAmount(consolidated?.balance || 0)}</h1>
+            <h1>{formatAmount(sharedReport?.balance || 0)}</h1>
           </Card>
         </CardContainer>
         <FormContainer>
           <Form ref={formRef} onSubmit={handleSubmit}>
+            <Select
+              icon={HiOutlineSelector}
+              name="balanceType"
+              options={constants.sharedBalanceTypes}
+              placeholder="Select type"
+            />
             <Input icon={MdDateRange} name="month" type="month" />
             <Button type="submit">Search</Button>
           </Form>
         </FormContainer>
-        {consolidated && (
+        {sharedReport && (
           <TableContainer>
-            {consolidated.requester.payments.length > 0 && (
+            {sharedReport.requester[shareType].length > 0 && (
               <Table>
                 <table>
                   <thead>
                     <tr>
-                      <th colSpan={2}>{consolidated.requester.name}</th>
+                      <th colSpan={2}>{sharedReport.requester.name}</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {consolidated.requester.payments.map((payment) => (
-                      <Fragment key={payment.id}>
-                        <tr>
-                          <td className="payment-type" colSpan={2}>{payment.description}</td>
-                        </tr>
-                        {payment.banks.map((bank) => (
-                          <tr key={bank.id}>
-                            <td className="bank-name">{bank.name}</td>
-                            <td className="bank-total">{formatAmount(bank.total)}</td>
-                          </tr>
-                        ))}
-                        <tr>
-                          <td className="requester" colSpan={2}>
-                            Total
-                            {' - '}
-                            {formatAmount(payment.total)}
-                          </td>
-                        </tr>
-                      </Fragment>
-                    ))}
+                    {shareType === 'payments' ? (
+                      <Payments payments={sharedReport.requester.payments} className="requester" />
+                    ) : (
+                      <Categories categories={sharedReport.requester.categories} className="requester" />
+                    )}
                   </tbody>
                 </table>
               </Table>
             )}
-            {consolidated.partner && (
+            {sharedReport.partner && (
               <Table>
                 <table className="table-partner">
                   <thead>
                     <tr>
-                      <th colSpan={2}>{consolidated.partner.name}</th>
+                      <th colSpan={2}>{sharedReport.partner.name}</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {consolidated.partner.payments.map((payment) => (
-                      <Fragment key={payment.id}>
-                        <tr>
-                          <td className="payment-type" colSpan={2}>{payment.description}</td>
-                        </tr>
-                        {payment.banks.map((bank) => (
-                          <tr key={bank.id}>
-                            <td className="bank-name">{bank.name}</td>
-                            <td className="bank-total">{formatAmount(bank.total)}</td>
-                          </tr>
-                        ))}
-                        <tr>
-                          <td className="partner" colSpan={2}>
-                            Total
-                            {' - '}
-                            {formatAmount(payment.total)}
-                          </td>
-                        </tr>
-                      </Fragment>
-                    ))}
+                    {shareType === 'payments' ? (
+                      <Payments payments={sharedReport.partner.payments} className="partner" />
+                    ) : (
+                      <Categories categories={sharedReport.partner.categories} className="partner" />
+                    )}
                   </tbody>
                 </table>
               </Table>
@@ -145,4 +165,4 @@ const ConsolidatedBalance: React.FC = () => {
   )
 }
 
-export default ConsolidatedBalance
+export default SharedBalance
